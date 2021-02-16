0b4e2c91921510253b8e159bf689b6c643a8ed18
gusfrehse <gusfrehse@gmail.com>
2021-02-16 00:27:59 -0300

    Add files via upload

---
+++ init.el
@@ -0,0 +1,8 @@
+;; remove ugly things :)
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1) 
+
+;; disable splash screen and startup message
+(setq inhibit-startup-message t) 
+(setq initial-scratch-message nil)
