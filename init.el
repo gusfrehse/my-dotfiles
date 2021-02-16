d305dca2c345d42ccf650bb6b09d91fa1eeff68f
gusfrehse <gusfrehse@gmail.com>
2021-02-16 00:39:02 -0300

    Add files via upload

--- init.el
+++ init.el
@@ -6,3 +6,9 @@
 ;; disable splash screen and startup message
 (setq inhibit-startup-message t) 
 (setq initial-scratch-message nil)
+
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
