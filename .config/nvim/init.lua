a9c0581308bc57eb70a592d472a98f7662412cc1
Gustavo Silveira Frehse <gusfrehse@gmail.com>
2025-10-27 09:21:43 -0300

    Add files via upload

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,11 +2,18 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.signcolumn = 'auto:1-9'
+vim.opt.signcolumn = 'yes:1'
 
-vim.lsp.enable { 'clangd', 'ts_ls' }
+vim.lsp.enable { 'clangd', 'ts_ls', 'eslint', 'tailwindcss' }
 
 vim.diagnostic.config({
-    virtual_lines = true
+    --virtual_lines = true
 })
 
+vim.cmd [[ colorscheme retrobox ]]
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NonText", { bg = "none" })
